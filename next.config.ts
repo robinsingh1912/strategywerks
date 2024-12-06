7fcf1c8b03f5349ec90e3d8991078c39a98482e4
robin singh <robinsingh1912@gmail.com>
2024-12-06 16:11:25 +0530

    feat: config updated

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   output: 'export', // <=== enables static exports
-  basePath: '/strategywerks',
-
   images: {
     domains: ['cdn.dummyjson.com'],
     unoptimized: true,
