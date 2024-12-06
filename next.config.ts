48b3d3521550d409859ef42a13c13a197441ae76
robin singh <robinsingh1912@gmail.com>
2024-12-06 15:59:44 +0530

    feat: updated config

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,12 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: 'export', // <=== enables static exports
+  basePath: '/strategywerks',
+
   images: {
     domains: ['cdn.dummyjson.com'],
+    unoptimized: true,
   },
 };
 
